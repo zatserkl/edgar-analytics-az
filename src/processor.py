c2f666e35d60b28a06be6f11865d4e9c33f7bdf1
zatserkl <zatserkl@gmail.com>
2018-04-04 18:49:57 -0700

    introduced process_request

--- src/processor.py
+++ src/processor.py
@@ -7,22 +7,43 @@ import datetime
 
 
 class User:
+    """ Keeps the number and times of requests.
+    Attributes:
+        inactivity_period (timedelta): class variable common for all instances
+        session_start (datetime): Time of the first request
+        session_write (datetime): Time to write to file if no more requests
+    """
+    inactivity_period = datetime.timedelta()  # class variable, common for all
+
     def __init__(self):
         # times are in datatime.
         self.session_start = 0  # datatime of the first request
         self.session_write = 0  # time slot to write to file if no more request
-        self.session_last = 0   # will be eliminated in final version
+        # self.session_last = 0   # will be eliminated in final version
+        self.nrequests = 0
 
-    def session_length(self, inactivity_period):
-        last_request = self.session_write - inactivity_period
+    def __str__(self):
+        s = ("session_start = " + str(self.session_start) +
+             " session_write = " + str(self.session_write) +
+             " nrequests = " + str(self.nrequests))
+        return s
+
+    def session_length(self):
+        last_request = self.session_write - User.inactivity_period
         deltat = last_request - self.session_start + 1
         deltat_seconds = math.ceil(deltat.total_seconds())
         return deltat_seconds
 
+    def process_request(self, date_time):
+        if self.nrequests == 0:
+            self.session_start = date_time
+        self.session_write = date_time + User.inactivity_period
+        self.nrequests += 1
+
 
 class Processor:
     def __init__(self, fname_input, inactivity_period, fname_output):
-        self.inactivity_period = inactivity_period
+        User.inactivity_period = datetime.timedelta(seconds=inactivity_period)
 
         self.dataStream = DataStream(fname_input)
         self.outfile = open(fname_output, "w")
@@ -36,5 +57,5 @@ class Processor:
     def process_request(self, ip, date_time):
         """ Processes the request from ip at datetime date_time
         """
-        self.userDict[ip].date_time = date_time
-        print(ip, self.userDict[ip].date_time)
+        self.userDict[ip].process_request(date_time)
+        print(self.userDict[ip])
